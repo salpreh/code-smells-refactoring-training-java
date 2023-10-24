0d8f75a5996e263b13d1cea677676f793b155ac3
salpreh <salva.perez46@gmail.com>
2023-10-24 11:57:55 +0200

    refactor: Direction checks symmetry

--- 01-refactoring-smelly-mars-rover/src/main/java/mars_rover/Rover.java
+++ 01-refactoring-smelly-mars-rover/src/main/java/mars_rover/Rover.java
@@ -16,54 +16,50 @@ public class Rover {
         for (int i = 0; i < commandsSequence.length(); ++i) {
             String command = commandsSequence.substring(i, i + 1);
 
-            if (command.equals("l") || command.equals("r")) {
+          if (command.equals("l")) {
 
-                // Rotate Rover
-                if (direction.equals("N")) {
-                    if (command.equals("r")) {
-                        direction = "E";
-                    } else {
-                        direction = "W";
-                    }
-                } else if (direction.equals("S")) {
-                    if (command.equals("r")) {
-                        direction = "W";
-                    } else {
-                        direction = "E";
-                    }
-                } else if (direction.equals("W")) {
-                    if (command.equals("r")) {
-                        direction = "N";
-                    } else {
-                        direction = "S";
-                    }
-                } else {
-                    if (command.equals("r")) {
-                        direction = "S";
-                    } else {
-                        direction = "N";
-                    }
-                }
+            // Rotate Rover
+            if (direction.equals("N")) {
+              direction = "W";
+            } else if (direction.equals("S")) {
+              direction = "E";
+            } else if (direction.equals("W")) {
+              direction = "S";
             } else {
-
-                // Displace Rover
-                int displacement1 = -1;
-
-                if (command.equals("f")) {
-                    displacement1 = 1;
-                }
-                int displacement = displacement1;
-
-                if (direction.equals("N")) {
-                    y += displacement;
-                } else if (direction.equals("S")) {
-                    y -= displacement;
-                } else if (direction.equals("W")) {
-                    x -= displacement;
-                } else {
-                    x += displacement;
-                }
+              direction = "N";
             }
+          } else if (command.equals("r")) {
+
+            // Rotate Rover
+            if (direction.equals("N")) {
+              direction = "E";
+            } else if (direction.equals("S")) {
+              direction = "W";
+            } else if (direction.equals("W")) {
+              direction = "N";
+            } else {
+              direction = "S";
+            }
+          } else {
+
+            // Displace Rover
+            int displacement1 = -1;
+
+            if (command.equals("f")) {
+              displacement1 = 1;
+            }
+            int displacement = displacement1;
+
+            if (direction.equals("N")) {
+              y += displacement;
+            } else if (direction.equals("S")) {
+              y -= displacement;
+            } else if (direction.equals("W")) {
+              x -= displacement;
+            } else {
+              x += displacement;
+            }
+          }
         }
     }
 
